dd66384b3468900fb2a27d3aeeff7b3ff29b15cc
zarif007 <zarifhuq786@gmail.com>
2023-08-06 18:34:10 +0600

    [MODIFIED]: enroll state

--- dist/app/modules/enrollState/enrollState.model.js
+++ dist/app/modules/enrollState/enrollState.model.js
@@ -1,32 +1,29 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.EnrollState = void 0;
-const mongoose_1 = require('mongoose');
-const EnrollStateSchema = new mongoose_1.Schema(
-  {
+const mongoose_1 = require("mongoose");
+const EnrollStateSchema = new mongoose_1.Schema({
     course: {
-      type: mongoose_1.Schema.Types.ObjectId,
-      ref: 'Course',
-      required: [true, 'The course ID is required'],
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: [true, 'The course ID is required'],
     },
     user: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     currentTopic: {
-      type: mongoose_1.Schema.Types.ObjectId,
-      ref: 'CourseTopic',
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'CourseTopic',
     },
     finishedTopics: {
-      type: [String],
-      default: [],
+        type: [String],
+        default: ['1'],
     },
-  },
-  {
+}, {
     timestamps: true,
     toJSON: {
-      virtuals: true,
+        virtuals: true,
     },
-  }
-);
+});
 exports.EnrollState = (0, mongoose_1.model)('EnrollState', EnrollStateSchema);
