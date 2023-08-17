7166be2745b3f1bf8b7793248619564e8d6365cf
zarif007 <zarifhuq786@gmail.com>
2023-08-17 22:58:42 +0600

    [FIXED]: ratings part

--- dist/app/modules/course/course.route.js
+++ dist/app/modules/course/course.route.js
@@ -1,15 +1,12 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.CourseRoutes = void 0;
-const express_1 = require('express');
-const course_controller_1 = require('./course.controller');
+const express_1 = require("express");
+const course_controller_1 = require("./course.controller");
 const router = (0, express_1.Router)();
 router.get('/', course_controller_1.CourseController.getCourses);
 router.get('/byId/:id', course_controller_1.CourseController.getSingleCourse);
-router.get(
-  '/bySlug/:slug',
-  course_controller_1.CourseController.getSingleCourseBySlug
-);
+router.get('/bySlug/:slug', course_controller_1.CourseController.getSingleCourseBySlug);
 router.post('/', course_controller_1.CourseController.createCourse);
 router.delete('/:id');
 router.patch('/:id', course_controller_1.CourseController.updateCourse);
--- dist/app/modules/user/user.service.js
+++ dist/app/modules/user/user.service.js
@@ -1,55 +1,25 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserService = void 0;
-const user_model_1 = require('./user.model');
-const getUserByExternalId = externalId =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const user_model_1 = require("./user.model");
+const getUserByExternalId = (externalId) => __awaiter(void 0, void 0, void 0, function* () {
     const user = yield user_model_1.User.findOne({ externalId });
     return user;
-  });
-const upsertUser = payload =>
-  __awaiter(void 0, void 0, void 0, function* () {
-    const user = yield user_model_1.User.findOneAndUpdate(
-      { externalId: payload.externalId },
-      payload,
-      { upsert: true, new: true, setDefaultsOnInsert: true }
-    );
+});
+const upsertUser = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield user_model_1.User.findOneAndUpdate({ externalId: payload.externalId }, payload, { upsert: true, new: true, setDefaultsOnInsert: true });
     return user;
-  });
+});
 exports.UserService = {
-  getUserByExternalId,
-  upsertUser,
+    getUserByExternalId,
+    upsertUser,
 };
