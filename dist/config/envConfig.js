32468bce7ca9e9ef272bb2ca567abc19560c40fe
zarif007 <zarifhuq786@gmail.com>
2023-08-30 01:17:24 +0600

    [ADDED]: spaces between clg

--- dist/config/envConfig.js
+++ dist/config/envConfig.js
@@ -1,14 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const dotenv_1 = __importDefault(require('dotenv'));
+const path_1 = __importDefault(require('path'));
 dotenv_1.default.config({ path: path_1.default.join(process.cwd(), '.env') });
 exports.default = {
-    node_env: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    clerk_secret_key: process.env.CLERK_SECRET_KEY,
+  node_env: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  clerk_secret_key: process.env.CLERK_SECRET_KEY,
 };
